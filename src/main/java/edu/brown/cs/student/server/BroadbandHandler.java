4c9a62064400c921ca9d91de8dfac1f9b9bfee20
JayRGopal <jay2003@me.com>
2024-02-13 17:13:42 -0500

    feat: basic handler for broadband query

--- src/main/java/edu/brown/cs/student/server/BroadbandHandler.java
+++ src/main/java/edu/brown/cs/student/server/BroadbandHandler.java
@@ -4,6 +4,33 @@ package edu.brown.cs.student.server;
 // State and county
 // Uses ACSDataSource to take in a state and county, and output percent, date time, state, county, etc.
 
+import edu.brown.cs.student.activity.Census;
+import edu.brown.cs.student.activity.CensusAPIUtilities;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import spark.Request;
+import spark.Response;
+
 public class BroadbandHandler {
 
+
+  @Override
+  public Object handle(Request request, Response response) throws Exception {
+    Set<String> params = request.queryParams();
+    String stateName = request.queryParams("state");
+    String countyName = request.queryParams("county");
+
+    ACSDataSource dataNow = newACSDataSource();
+
+    } catch (Exception e) {
+      e.printStackTrace();
+      // This is a relatively unhelpful exception message. An important part of this sprint will be
+      // in learning to debug correctly by creating your own informative error messages where Spark
+      // falls short.
+      responseMap.put("result", "Exception");
+    }
+    return responseMap;
+
+  }
 }

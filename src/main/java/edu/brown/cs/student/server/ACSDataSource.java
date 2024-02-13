dd12cc7f5e7c8de368d6fde6b52e6bd986a98a4c
JayRGopal <jay2003@me.com>
2024-02-13 17:13:30 -0500

    feat: sendRequest generic and ACSDataSource skeletons

--- src/main/java/edu/brown/cs/student/server/ACSDataSource.java
+++ src/main/java/edu/brown/cs/student/server/ACSDataSource.java
@@ -1,6 +1,13 @@
 package edu.brown.cs.student.server;
 
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class ACSDataSource {
 
   // Specify yes or no caching
@@ -8,5 +15,36 @@ public class ACSDataSource {
   // 1 - retrieve broadband data with caching
   // 2 - retrieve w/o caching
 
+  private final static String API_KEY = "47f9ee8e9ab596f0aec07dae474192f8a895fd54";
+
+  private static Object broadbandNoCache(){
+    // 3 API calls to ACS API
+    return null;
+  }
+
+  private static Object broadbandWithCache(){
+
+    return null;
+  }
+
+  private String sendRequest(
+      String urlNOW
+  ) throws URISyntaxException, IOException, InterruptedException {
+    // Build a request to ACS API
+    HttpRequest buildCensusApiRequest =
+        HttpRequest.newBuilder()
+            .uri(new URI(urlNOW))
+            .GET()
+            .build();
+
+    // Send that API request then store the response in this variable. Note the generic type.
+    HttpResponse<String> sentCensusApiResponse =
+        HttpClient.newBuilder()
+            .build()
+            .send(buildCensusApiRequest, HttpResponse.BodyHandlers.ofString());
+
+
+    return sentCensusApiResponse.body();
+  }
 
 }
